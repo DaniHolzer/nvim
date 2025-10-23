5cebbcdbfa954d20fae9a275a9d918e80a4123ef
DaniHolzer <holzerdaniel01@gmail.com>
2025-10-23 17:59:44 +0200

    WIP fix nvim plugins

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -8,61 +8,58 @@ return{
     event = { "BufReadPre", "BufNewFile" },
     config = function()
 
--- Set up highlight groups:
-local highlight = {
-  'IndentBlanklineChar',
-}
+      local indent_blankline = require("ibl")
 
--- Pass colors to highlight groups:
-local hooks = require('ibl.hooks')
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#252525' })
-  vim.api.nvim_set_hl(0, 'hl-IblScope', { fg = '#606366' })
-end)
+      -- Set up highlight groups:
+      local highlight = {
+        'IndentBlanklineChar',
+      }
 
-    local indent_blankline = require ('ibl')
+      -- Pass colors to highlight groups:
+      local hooks = require('ibl.hooks')
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#252525' })
+        vim.api.nvim_set_hl(0, 'hl-IblScope', { fg = '#606366' })
+      end)
 
-    indent_blankline.setup ({
-
-  indent = {
-    char = '▏',
-    highlight = highlight,
-  },
-  scope = {
-    enabled            = true,   -- 'true': Highlight only the intended indentation and the current scope by an underline.
-    show_start         = false,  -- Shows an underline on the first line of the scope.
-    show_end           = false,  -- Shows an underline on the last line of the scope.
-    show_exact_scope   = false,
-    injected_languages = true,
-  },
-  exclude = {
-    filetypes = {
-      'help',
-      'packer',
-      'NvimTree',
-      'Trouble',
-      'dashboard',
-      'TelescopePrompt',
-      'TelescopeResults',
-      'TelescopePreviewer',
-      'lspinfo',
-      'startify',
-      'fugitive',
-      'fugitiveblame',
-      'gitcommit',
-      'gitrebase',
-      'svn',
-      'hgcommit',
-    },
-    buftypes = {
-      'terminal',
-      'nofile',
-      'quickfix',
-      'prompt',
-    },
-  },
-
-})
-
-end,
+      indent_blankline.setup ({
+        indent = {
+          char = { text = '▏' },
+          highlight = highlight,
+        },
+        scope = {
+          enabled            = true,   -- 'true': Highlight only the intended indentation and the current scope by an underline.
+          show_start         = false,  -- Shows an underline on the first line of the scope.
+          show_end           = false,  -- Shows an underline on the last line of the scope.
+          show_exact_scope   = false,
+          injected_languages = true,
+        },
+        exclude = {
+          filetypes = {
+            'help',
+            'packer',
+            'NvimTree',
+            'Trouble',
+            'dashboard',
+            'TelescopePrompt',
+            'TelescopeResults',
+            'TelescopePreviewer',
+            'lspinfo',
+            'startify',
+            'fugitive',
+            'fugitiveblame',
+            'gitcommit',
+            'gitrebase',
+            'svn',
+            'hgcommit',
+          },
+          buftypes = {
+            'terminal',
+            'nofile',
+            'quickfix',
+            'prompt',
+          },
+        },
+      })
+  end,
 }
