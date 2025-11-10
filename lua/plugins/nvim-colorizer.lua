ebe9c10b7b6ee35da7a37e01ae93e32b62a4f31a
DaniHolzer <holzerdaniel01@gmail.com>
2025-11-10 17:17:30 +0100

    Add color highlighting for rgb(...)

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -10,10 +10,10 @@ return {
     vim.opt.termguicolors = true
 
     local colorizer = require("colorizer")
-    colorizer.setup{
-        -- Enable highlighting in all file types:
-        '*',
-    }
+    colorizer.setup(
+        { '*'},               -- Enable highlighting in all file types:
+        { css = true; }       -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    )
     vim.cmd([[
       augroup Colorizer
         autocmd!
